111d526747952073e9d51a430902b5ff6e943aa0
Theo <109154113+Tixeo@users.noreply.github.com>
2025-03-03 00:16:26 +0100

    Update API

--- app/api/articles/delete/route.ts
+++ app/api/articles/delete/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server"
 import Airtable from 'airtable'
 
-const AIRTABLE_API_KEY = 'patVUcCjJfP3NMVVq.ef8f3c5194cd4c7f6a3bdfc0a117ec403a022dc52a3544070177dae86655678f'
+const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY_ADMIN;
 const AIRTABLE_BASE_ID = process.env.AIRTABLE_BASE_ID
 
 const base = new Airtable({ apiKey: AIRTABLE_API_KEY }).base(AIRTABLE_BASE_ID!)
